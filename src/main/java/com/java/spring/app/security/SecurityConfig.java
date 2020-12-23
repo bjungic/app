d2dfa1852664396674e36778421793cd65b88640
bjungic <boki.kibo@gmail.com>
2020-12-23 14:54:42 +0100

    nista ne radi

--- src/main/java/com/java/spring/app/security/SecurityConfig.java
+++ src/main/java/com/java/spring/app/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/**").hasAnyAuthority("ADMIN", "USER")
                 .antMatchers("/d").hasAnyAuthority("USER", "ADMIN")
