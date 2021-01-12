818a51fa1ed5092952442343dc3918e6551cfc32
bjungic <boki.kibo@gmail.com>
2021-01-12 16:50:24 +0100

    0.5.2

--- src/main/java/com/java/spring/app/services/UserRepository.java
+++ src/main/java/com/java/spring/app/services/UserRepository.java
@@ -1,7 +1,11 @@
 package com.java.spring.app.services;
 
 import com.java.spring.app.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query(value = "select * from users where username = ?1", nativeQuery = true)
+    User getUserByUsername(String username);
 }
