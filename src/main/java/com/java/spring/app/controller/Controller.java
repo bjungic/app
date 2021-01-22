d5766ff341fa9c57dea15e953cbf296bcecd4bd5
bjungic <boki.kibo@gmail.com>
2021-01-22 14:49:53 +0100

    0.5.5

--- src/main/java/com/java/spring/app/controller/Controller.java
+++ src/main/java/com/java/spring/app/controller/Controller.java
@@ -1,11 +1,9 @@
 package com.java.spring.app.controller;
 
-import io.micrometer.core.instrument.util.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.time.Instant;
 import java.time.ZoneId;
@@ -23,9 +21,7 @@ public class Controller {
 
     @GetMapping
     public String index() throws FileNotFoundException {
-        FileInputStream fileInputStream = new FileInputStream("/home/bjungic/aplikacija/app.log");
-        String log = IOUtils.toString(fileInputStream);
         Instant time = Instant.now();
-        return "<h1>bjungic <br><br>" + dateTimeFormatter.format(time) + "</h1><br><br>" + log;
+        return "<h1>bjungic <br><br>" + dateTimeFormatter.format(time) + "</h1>";
     }
 }
