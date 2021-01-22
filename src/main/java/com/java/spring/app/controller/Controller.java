a84ac974722d236fc0411a7f095d3d5ae6e9fbb3
bjungic <boki.kibo@gmail.com>
2021-01-22 14:36:01 +0100

    0.5.5

--- src/main/java/com/java/spring/app/controller/Controller.java
+++ src/main/java/com/java/spring/app/controller/Controller.java
@@ -1,9 +1,12 @@
 package com.java.spring.app.controller;
 
+import io.micrometer.core.instrument.util.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -17,9 +20,12 @@ public class Controller {
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
             .withLocale(Locale.forLanguageTag("hr-HR"))
             .withZone(ZoneId.systemDefault());
+
     @GetMapping
-    public String index() {
+    public String index() throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("/home/bjungic/aplikacija/app.log");
+        String log = IOUtils.toString(fileInputStream);
         Instant time = Instant.now();
-        return "<h1>bjungic <br><br>" + dateTimeFormatter.format(time) + "</h1>";
+        return "<h1>bjungic <br><br>" + dateTimeFormatter.format(time) + "</h1><br><br>" + log;
     }
 }
